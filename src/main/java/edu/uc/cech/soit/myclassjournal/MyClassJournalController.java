18d686e2b55f860c8c9aa8919fb745a7c325245d
bartrn <89704134+bartrn@users.noreply.github.com>
2022-06-12 15:02:40 -0400

    checkpoint 3 start

--- src/main/java/edu/uc/cech/soit/myclassjournal/MyClassJournalController.java
+++ src/main/java/edu/uc/cech/soit/myclassjournal/MyClassJournalController.java
@@ -3,14 +3,19 @@ package edu.uc.cech.soit.myclassjournal;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 public class MyClassJournalController {
+    /** html */
+    /** checkpoint 3 */
+
     /**
      * Handle the / endpoint
      * @return
      */
     @RequestMapping("/")
+
+    
     public String index() {
         return "start";
-    }
+    }/** html code */
 
 
 }
